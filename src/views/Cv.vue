64cbf5a5a21b4fd6cd672250fce2cd9936c6fc92
JrogeT <jorgerodrigotorrez@gmail.com>
2022-08-28 00:21:30 -0400

    CV view updated to responsive

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -1,7 +1,7 @@
 <!--suppress HtmlUnknownTarget -->
 <template>
   <div class="row bg-light">
-    <iframe class="col-9 mx-auto" src="JorgeTorrezCV-en.pdf#toolbar=0" style="height: 75vh;"/>
+    <iframe class="col-md-9 mx-auto" src="JorgeTorrezCV-en.pdf#toolbar=0" style="height: 75vh;"/>
   </div>
   <div class="row bg-light pt-3">
     <a class="justify-content-center d-flex" href="JorgeTorrezCV-en.pdf" download>
