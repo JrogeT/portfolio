d9244dfa9c361ead619a0545626fb723f9e9b9cb
JrogeT <jorgerodrigotorrez@gmail.com>
2022-08-28 00:17:40 -0400

    CV view updated

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -1,12 +1,20 @@
+<!--suppress HtmlUnknownTarget -->
 <template>
-  CV
+  <div class="row bg-light">
+    <iframe class="col-9 mx-auto" src="JorgeTorrezCV-en.pdf#toolbar=0" style="height: 75vh;"/>
+  </div>
+  <div class="row bg-light pt-3">
+    <a class="justify-content-center d-flex" href="JorgeTorrezCV-en.pdf" download>
+      <span class="badge text-bg-dark">
+        Download
+      </span>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "AppCv",
-  components: {
-  }
+  name: "AppCv"
 }
 </script>
 
